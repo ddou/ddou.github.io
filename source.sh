ec503bc6210241bc656c29d058aa07fbab346e39
Dou Yutao <yutaodou@gmail.com>
2020-04-22 12:47:56 +0800

    clean

--- source.sh
+++ source.sh
@@ -2,6 +2,6 @@
 echo "Uploading source ..."
 git add --all 1>/dev/null
 git commit -m "Update source" 1>/dev/null
-git push -f origin master:source 1>/dev/null
+git push -f origin source:source 1>/dev/null
 
 echo "DONE."
