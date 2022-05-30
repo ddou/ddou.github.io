d7afee97f41077f9b30283514d991090ea6f5fa2
Dou Yutao <yutaodou@gmail.com>
2022-05-30 22:00:06 +0800

    Update site

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ echo "Uploading new site..."
 cd public 
 git add --all 1>/dev/null
 git commit -m "Update site"  1>/dev/null
-git push origin -f main:master 1>/dev/null
+git push origin -f master 1>/dev/null
+
 
 echo "DONE."
